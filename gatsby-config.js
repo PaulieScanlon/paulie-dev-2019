1a27bc3f5a1d3c4c7979db3319e0cb22b0f82cf1
pauliescanlon <pauliescanlon@gmail.com>
2019-11-12 11:12:47 +0000

    add @paulescanlon/gatsby-theme-gatstats

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,15 @@
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-theme-blog`,
-      options: {},
+      resolve: `@pauliescanlon/gatsby-theme-gatstats`,
     },
   ],
-  // Customize your site metadata:
+
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
-    social: [
-      {
-        name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/gatsbyjs`,
-      },
-    ],
+    title: "Gat Stats",
+    description: "Data viz for blogs",
+    config: {
+      sideBarWidth: 244,
+    },
   },
 }
