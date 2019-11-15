de46d9349bfb7c76e13a9b56951a899bb5c0e5e8
pauliescanlon <pauliescanlon@gmail.com>
2019-11-15 09:24:27 +0000

    upgrade to 0.0.14

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 module.exports = {
+  siteMetadata: {
+    title: "Gat Stats",
+    description: "Data viz for blogs",
+    keywords: ["GatsbyJs", "React", "theme-ui"],
+    config: {
+      sideBarWidth: 244,
+    },
+  },
   plugins: [
     {
       resolve: `@pauliescanlon/gatsby-theme-gatstats`,
     },
   ],
-
-  siteMetadata: {
-    title: "Gat Stats",
-    description: "Data viz for blogs",
-    config: {
-      sideBarWidth: 244,
-    },
-  },
 }
