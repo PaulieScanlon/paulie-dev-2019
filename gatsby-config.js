024002c5cc61843843b783273fe87c9ae9a88477
pauliescanlon <pauliescanlon@gmail.com>
2020-02-02 20:15:05 +0000

    update profile

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "Paul Scanlon",
     description:
-      "I'm a React UI developer / UX Engineer (contract) based in Essex. React, GatsbyJs, JavaScript, TypeScript/Flow, StyledComponents, Storybook, TDD (Jest/Enzyme) and a tiny bit of Node.js.",
+      "I'm a React UI developer / UX Engineer (contract). React, GatsbyJs, JavaScript, TypeScript/Flow, StyledComponents, Storybook, TDD (Jest/Enzyme) and a tiny bit of Node.js.",
     keywords: [
       "React",
       "GatsbyJs",
