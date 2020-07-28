613b92d189a0ed6d4329db734a280f31ec680602
pauliescanlon <pauliescanlon@gmail.com>
2020-07-28 15:13:40 +0100

    add mergeScriptHashes and mergeStyleHashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,9 +59,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-csp`,
       options: {
+        mergeScriptHashes: true,
+        mergeStyleHashes: true,
         directives: {
-          "script-src": `'self' data: www.google-analytics.com`,
-          "style-src": `'self' fonts.googleapis.com fonts.gstatic.com`,
+          "script-src": `'self' 'unsafe-inline' data: www.google-analytics.com`,
+          "style-src": `'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com`,
           "img-src": `'self' data: www.google-analytics.com`,
           "font-src": `'self' data: fonts.gstatic.com`,
         },
