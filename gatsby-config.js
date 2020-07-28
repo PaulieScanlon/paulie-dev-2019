a8ded77b5c72bb242e8110443458dffc6b59c856
pauliescanlon <pauliescanlon@gmail.com>
2020-07-28 13:54:24 +0100

    add gatsby-plugin-csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,5 +58,20 @@ module.exports = {
         trackingId: "UA-76055934-4",
       },
     },
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
+        directives: {
+          "script-src": "'self' www.google-analytics.com",
+          "style-src": "'self' 'unsafe-inline'",
+          "img-src": "'self' data: www.google-analytics.com",
+        },
+      },
+    },
   ],
 }
