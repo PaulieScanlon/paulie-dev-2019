e643789cb25360f4bf973c7a10ff7c5b2584dc7c
pauliescanlon <pauliescanlon@gmail.com>
2021-01-12 12:14:55 +0000

    add theme-ui-alpha-1 post

---
+++ src/posts/2021/01/MrJsx-alpha-1.js
@@ -0,0 +1,15 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+
+export const MrJsx = () => {
+  return (
+    <div
+      sx={{
+        color: "text",
+        backgroundColor: "surface",
+      }}
+    >
+      MrJsx
+    </div>
+  )
+}
