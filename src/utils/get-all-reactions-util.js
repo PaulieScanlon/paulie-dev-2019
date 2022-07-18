ae3f36b4c0c6a32cbe348c8e140704a71eb1a978
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-07-18 19:58:38 +0100

    feat: abstract get-all-reactions and re-use in serverless function and SSR serverData

---
+++ src/utils/get-all-reactions-util.js
@@ -0,0 +1,25 @@
+const faunadb = require('faunadb');
+const { reactionsByAmount } = require('./reactions-by-amount');
+
+module.exports.get = async function () {
+  const q = faunadb.query;
+
+  const client = new faunadb.Client({ secret: process.env.FAUNA_KEY });
+
+  try {
+    const response = await client.query(
+      q.Paginate(q.Match(q.Index(`all_reactions_${process.env.NODE_ENV}`)), { size: 1000 })
+    );
+
+    const result = response.data.map(([ref, slug, reaction, date]) => ({
+      ref: ref.id,
+      slug,
+      reaction,
+      date
+    }));
+
+    return { message: 'all reaction', reactions: reactionsByAmount(result) };
+  } catch (error) {
+    return { message: error.message };
+  }
+};
