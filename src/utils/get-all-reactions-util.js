84212d50261b74e8bed3872a3a62ac20afedf33c
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-07-24 09:27:24 +0100

    feat: add reaction accordion

--- src/utils/get-all-reactions-util.js
+++ src/utils/get-all-reactions-util.js
@@ -1,5 +1,4 @@
 const faunadb = require('faunadb');
-const { reactionsByAmount } = require('./reactions-by-amount');
 
 module.exports.get = async function () {
   const q = faunadb.query;
@@ -8,8 +7,8 @@ module.exports.get = async function () {
 
   try {
     const response = await client.query(
-      // q.Paginate(q.Match(q.Index(`all_reactions_${process.env.NODE_ENV}`)), { size: 1000 })
-      q.Paginate(q.Match(q.Index('all_reactions_production')), { size: 1000 })
+      q.Paginate(q.Match(q.Index(`all_reactions_${process.env.NODE_ENV}`)), { size: 1000 })
+      // q.Paginate(q.Match(q.Index('all_reactions_production')), { size: 1000 })
     );
 
     const result = response.data.map(([ref, slug, reaction, date]) => ({
@@ -19,9 +18,49 @@ module.exports.get = async function () {
       date
     }));
 
+    const groupBy = (arr, key) => {
+      return arr.reduce(function (rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+      }, {});
+    };
+
+    const format = (array) => {
+      return result
+        .filter((item) => {
+          if (item.slug !== null) {
+            return item;
+          }
+        })
+        .map((item) => {
+          const { reaction, date, slug } = item;
+          return {
+            reaction,
+            date,
+            slug
+          };
+        });
+    };
+
+    const formatted = format(result);
+
+    const byReaction = groupBy(formatted, 'reaction');
+
+    const grouped = Object.keys(byReaction)
+      .filter((key) => key !== null)
+      .map((key) => {
+        const bySlug = groupBy(byReaction[key], 'slug');
+
+        return {
+          title: key,
+          total: byReaction[key].length,
+          posts: bySlug
+        };
+      });
+
     return {
       message: 'all reactions',
-      reactions: reactionsByAmount(result)
+      reactions: grouped
     };
   } catch (error) {
     return { message: error.message };
