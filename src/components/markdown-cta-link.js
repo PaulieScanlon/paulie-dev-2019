10344dfe3b45061cafeb275b144a4b5d0785125f
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-12 23:12:02 +0100

    docs: add 500 bottles demo

--- src/components/markdown-cta-link.js
+++ src/components/markdown-cta-link.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 const MarkdownCtaLink = ({ children, href }) => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex my-16 items-center justify-center">
       <a
         href={href}
         target="_blank"
         rel="noreferrer"
-        className="inline-flex flex-col items-center gap-2 no-underline text-tertiary text-center py-2 px-4 transition-all duration-300 rounded border border-tertiary/40 bg-surface hover:text-white hover:bg-tertiary/20"
+        className="inline-flex flex-col items-center gap-2 no-underline text-tertiary text-center py-2 px-4 transition-all duration-300 rounded border border-tertiary/40 bg-surface hover:text-white hover:bg-tertiary/10"
       >
         {children}
       </a>
