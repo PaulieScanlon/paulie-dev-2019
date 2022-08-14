1518261e44d688c25fe96cc26c1604a7de03bf14
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-14 14:16:08 +0100

    refactor: downgrade packages add globe to dashboard

--- src/components/three-scene.js
+++ src/components/three-scene.js
@@ -10,22 +10,21 @@ import ThreeLatLong from './three-lat-long';
 import ThreeSphere from './three-sphere';
 
 const ThreeScene = ({ locations }) => {
+  if (!locations) return null;
   return (
-    <div className="absolute w-full h-[36rem] z-0">
-      <Canvas
-        camera={{
-          fov: 75,
-          position: [0, 0, 2]
-        }}
-      >
-        <OrbitControls enableRotate={true} enableZoom={false} enablePan={false} />
-        <ambientLight intensity={2} />
-        <ThreeLatLong locations={locations} />
-        <ThreeGeo />
-        <ThreeGraticule />
-        <ThreeSphere />
-      </Canvas>
-    </div>
+    <Canvas
+      camera={{
+        fov: 75,
+        position: [0, 0, 2]
+      }}
+    >
+      <OrbitControls enableRotate={true} enableZoom={false} enablePan={false} />
+      <ambientLight intensity={2} />
+      <ThreeLatLong locations={locations} />
+      <ThreeGeo />
+      <ThreeGraticule />
+      <ThreeSphere />
+    </Canvas>
   );
 };
 
