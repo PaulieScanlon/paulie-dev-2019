0e918ec87dbf7e3a8a87c5e754f50794cd626797
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-14 18:40:14 +0100

    test: remove three-geo-json-geometry d3

--- src/components/three-graticlue.js
+++ src/components/three-graticlue.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { GeoJsonGeometry } from 'three-geojson-geometry';
-import * as d3 from 'd3';
+// import { GeoJsonGeometry } from 'three-geojson-geometry';
+// import * as d3 from 'd3';
 
 const ThreeGraticule = () => {
-  return (
-    <lineSegments geometry={new GeoJsonGeometry(d3.geoGraticule10(), 1.03)}>
-      <lineBasicMaterial color="#2c2a56" />
-    </lineSegments>
-  );
+  return null;
+  // return (
+  //   <lineSegments geometry={new GeoJsonGeometry(d3.geoGraticule10(), 1.03)}>
+  //     <lineBasicMaterial color="#2c2a56" />
+  //   </lineSegments>
+  // );
 };
 
 export default ThreeGraticule;
