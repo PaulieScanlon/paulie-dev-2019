485f6440aecd14050a3f4b34f2775bd7c4cc35eb
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-14 20:25:05 +0100

    feat: add googleapis

---
+++ src/utils/get-lat-long-google-ua.util.js
@@ -0,0 +1,48 @@
+const { google } = require('googleapis');
+
+const jwt = new google.auth.JWT(
+  process.env.GOOGLE_ANALYTICS_CLIENT_EMAIL,
+  null,
+  process.env.GOOGLE_ANALYTICS_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+  'https://www.googleapis.com/auth/analytics.readonly'
+);
+
+module.exports.get = async function () {
+  try {
+    await jwt.authorize();
+    const response = await google.analytics('v3').data.ga.get({
+      auth: jwt,
+      ids: `ga:${process.env.GOOGLE_ANALYTICS_VIEW_ID}`,
+      'start-date': '2019-10-01',
+      // 'start-date': '7daysAgo',
+      'end-date': 'today',
+      metrics: 'ga:pageviews',
+      dimensions: 'ga:city,ga:latitude,ga:longitude,ga:country,ga:countryIsoCode'
+    });
+
+    return {
+      message: 'A ok!',
+      data: response.data.rows
+        .map(([city, lat, lng, country, countryIsoCode, count]) => {
+          return {
+            city: city,
+            lat: lat,
+            lng: lng,
+            country: country,
+            country_code: countryIsoCode,
+            count: count
+          };
+        })
+        .sort((a, b) => a.country.localeCompare(b.country))
+        .filter((item) => {
+          if (item.country === '(not set)' || item.city === '(not set)') {
+            return null;
+          } else {
+            return item;
+          }
+        })
+    };
+  } catch (error) {
+    return { message: error.message };
+  }
+};
