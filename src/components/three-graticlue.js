b3626cca2506475f1e33843c9e28ffdd720a2961
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-14 19:28:48 +0100

    test: add d3-geo

--- src/components/three-graticlue.js
+++ src/components/three-graticlue.js
@@ -1,14 +1,13 @@
 import React from 'react';
-// import { GeoJsonGeometry } from 'three-geojson-geometry';
-// import * as d3 from 'd3';
+import { GeoJsonGeometry } from 'three-geojson-geometry';
+import * as d3 from 'd3-geo';
 
 const ThreeGraticule = () => {
-  return null;
-  // return (
-  //   <lineSegments geometry={new GeoJsonGeometry(d3.geoGraticule10(), 1.03)}>
-  //     <lineBasicMaterial color="#2c2a56" />
-  //   </lineSegments>
-  // );
+  return (
+    <lineSegments geometry={new GeoJsonGeometry(d3.geoGraticule10(), 1.03)}>
+      <lineBasicMaterial color="#34324f" />
+    </lineSegments>
+  );
 };
 
 export default ThreeGraticule;
