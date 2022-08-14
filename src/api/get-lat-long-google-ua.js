378559bdd7869421716657def00a73725176a74a
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-14 10:38:46 +0100

    refactor: abstract locations function to shared util

--- src/api/get-lat-long-google-ua.js
+++ src/api/get-lat-long-google-ua.js
@@ -1,47 +1,10 @@
-const { google } = require('googleapis');
+const allLatLongGoogleUatil = require('../utils/get-lat-long-google-ua.util');
 
-const jwt = new google.auth.JWT(
-  process.env.GOOGLE_ANALYTICS_CLIENT_EMAIL,
-  null,
-  process.env.GOOGLE_ANALYTICS_PRIVATE_KEY.replace(/\\n/gm, '\n'),
-  'https://www.googleapis.com/auth/analytics.readonly'
-);
-
-export default async function (req, res) {
+export default async function handler(req, res) {
   try {
-    await jwt.authorize();
-    const response = await google.analytics('v3').data.ga.get({
-      auth: jwt,
-      ids: `ga:${process.env.GOOGLE_ANALYTICS_VIEW_ID}`,
-      'start-date': '2019-10-01',
-      // 'start-date': '7daysAgo',
-      'end-date': 'today',
-      metrics: 'ga:pageviews',
-      dimensions: 'ga:city,ga:latitude,ga:longitude,ga:country,ga:countryIsoCode'
-    });
+    const response = await allLatLongGoogleUatil.get();
 
-    res.status(200).json({
-      message: 'A ok!',
-      data: response.data.rows
-        .map(([city, lat, lng, country, countryIsoCode, count]) => {
-          return {
-            city: city,
-            lat: lat,
-            lng: lng,
-            country: country,
-            country_code: countryIsoCode,
-            count: count
-          };
-        })
-        .sort((a, b) => a.country.localeCompare(b.country))
-        .filter((item) => {
-          if (item.country === '(not set)' || item.city === '(not set)') {
-            return null;
-          } else {
-            return item;
-          }
-        })
-    });
+    res.status(200).json({ message: 'all ua-locations', data: response.data });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
