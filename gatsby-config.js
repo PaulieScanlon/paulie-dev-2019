c56e309f2cdb098f31b591b3370acc0308352e77
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-15 10:33:43 +0100

    test: source all demos mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/content/demos/2022`
+        path: `${__dirname}/content/demos/`
       }
     },
     {
