9db591e62c145451c8ec95730b5354103b2ff1cd
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-15 08:57:09 +0100

    test: remove three-json-geometry

--- src/components/three-geo.js
+++ src/components/three-geo.js
@@ -1,46 +1,47 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import { GeoJsonGeometry } from 'three-geojson-geometry';
-
-// import { data } from './three-ne_110m_admin_0_countries';
-
-const ThreeGeo = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [response, setResponse] = useState(null);
-
-  useEffect(() => {
-    const getJsonGeomery = async () => {
-      try {
-        const response = await (
-          await fetch('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_0_countries.geojson', {
-            method: 'GET'
-          })
-        ).json();
-
-        setIsLoading(false);
-        setResponse(response.features);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getJsonGeomery();
-  }, []);
-
-  return (
-    <Fragment>
-      {isLoading ? null : (
-        <Fragment>
-          {response.map(({ geometry }, index) => {
-            return (
-              <lineSegments key={index} geometry={new GeoJsonGeometry(geometry, 1)}>
-                <lineBasicMaterial color="#4b4582" />
-              </lineSegments>
-            );
-          })}
-        </Fragment>
-      )}
-    </Fragment>
-  );
-};
+const ThreeGeo = () => null;
 
 export default ThreeGeo;
+// import React, { Fragment, useEffect, useState } from 'react';
+// import { GeoJsonGeometry } from 'three-geojson-geometry';
+
+// const ThreeGeo = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [response, setResponse] = useState(null);
+
+//   useEffect(() => {
+//     const getJsonGeomery = async () => {
+//       try {
+//         const response = await (
+//           await fetch('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_0_countries.geojson', {
+//             method: 'GET'
+//           })
+//         ).json();
+
+//         setIsLoading(false);
+//         setResponse(response.features);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+
+//     getJsonGeomery();
+//   }, []);
+
+//   return (
+//     <Fragment>
+//       {isLoading ? null : (
+//         <Fragment>
+//           {response.map(({ geometry }, index) => {
+//             return (
+//               <lineSegments key={index} geometry={new GeoJsonGeometry(geometry, 1)}>
+//                 <lineBasicMaterial color="#4b4582" />
+//               </lineSegments>
+//             );
+//           })}
+//         </Fragment>
+//       )}
+//     </Fragment>
+//   );
+// };
+
+// export default ThreeGeo;
