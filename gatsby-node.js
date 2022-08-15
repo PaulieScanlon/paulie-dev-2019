39c082d7319704cf6e331989cf5e786fea08a89c
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-15 15:40:21 +0100

    test: defer if type not equal post

--- gatsby-node.js
+++ gatsby-node.js
@@ -153,8 +153,8 @@ exports.createPages = async ({ graphql, actions: { createPage, createRedirect } 
       context: {
         id: id
       },
-      // defer: type !== 'post' ? true : false
-      defer: false
+      defer: type !== 'post' ? true : false
+      // defer: false
       // defer: type === 'article' || type === 'stream' ? true : false
     });
   });
