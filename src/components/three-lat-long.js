04d728eab283ebbad184573b07ff30b8aae6a7fc
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-15 22:11:29 +0100

    feat: add threejs globe

---
+++ src/components/three-lat-long.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Points, Point } from '@react-three/drei';
+
+import * as THREE from 'three';
+
+const getVertex = (lat, lng, radius) => {
+  const vector = new THREE.Vector3().setFromSpherical(
+    new THREE.Spherical(radius, THREE.MathUtils.degToRad(90 - lat), THREE.MathUtils.degToRad(lng))
+  );
+  return vector;
+};
+
+const ThreeLatLong = ({ locations }) => {
+  return (
+    <group>
+      <Points>
+        <pointsMaterial vertexColors size={0.013} />
+        {locations
+          ? locations.map((data, index) => {
+              const { lat, lng } = data;
+              return <Point key={index} position={getVertex(lat, lng, 1.06)} color="#fd417a" />;
+            })
+          : null}
+      </Points>
+    </group>
+  );
+};
+
+ThreeLatLong.propTypes = {
+  /** Geographical Locations */
+  locations: PropTypes.any.isRequired
+};
+
+export default ThreeLatLong;
