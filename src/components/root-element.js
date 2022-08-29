139f2306b5c67b871b747e2ca6a38cf07f09d06b
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-29 09:39:14 +0100

    feat: add plausible

--- src/components/root-element.js
+++ src/components/root-element.js
@@ -4,10 +4,26 @@ import { Script } from 'gatsby';
 const RootElement = ({ children }) => {
   return (
     <Fragment>
+      {/* <Script
+        data-domain="paulie.dev"
+        src="https://plausible.io/js/script.js"
+        strategy="off-main-thread"
+        forward={['plausible']}
+      />
+      <Script
+        id="plausible-config"
+        strategy="off-main-thread"
+        dangerouslySetInnerHTML={{
+          __html: `window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`
+        }}
+      /> */}
+
+      <Script src="https://plausible.io/js/script.manual.js" strategy="post-hydrate" data-domain="paulie.dev" />
+
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GATSBY_GA_MEASUREMENT_ID}`}
         strategy="off-main-thread"
-        forward={[`gtag`]}
+        forward={['gtag']}
       />
       <Script
         id="gtag-config"
